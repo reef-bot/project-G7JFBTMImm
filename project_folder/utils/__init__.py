8e614b1c1768a76209a7f21e6c1c0f6d08fd0cc2
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-26 10:00:21 +0530

    Added: `project_folder/utils/__init__.py`

---
+++ project_folder/utils/__init__.py
@@ -0,0 +1,5 @@
+# project_folder/utils/__init__.py
+
+from .random_generator import generate_random_numbers
+
+# This file serves as the entry point for the utils package. It imports the necessary functions from random_generator.py.
